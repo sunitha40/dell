c7498701a4876c927ff4e7611cfb65704fe489dd
sunitha40 <106382090+sunitha40@users.noreply.github.com>
2022-11-15 18:15:41 +0530

    Update sample.py
    typo

--- sample.py
+++ sample.py
@@ -3,5 +3,5 @@ import streamlit
 import numpy as np
 import pandas as pd
 streamlit.text("hello")
-df = pd.read_csv("https://www.fruityvice.com/api/fruit/watermelon")
+df = pd.read_csv("abc.txt")
 streamlit.dataframe(df)
